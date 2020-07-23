04b4d0a01b47cb7e40c369cb60ced1d403cfb5af
Lenart Papež <lenartpapez9@gmail.com>
2020-07-23 17:28:29 +0200

    First commit.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,31 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'tiny': '.7rem'
+      },
+      colors: {
+        'grayish': '#EFEFEF',
+        'really-gray': '#A9A9A9',
+        'ultra-gray': '#787878',
+        'red-hover': '#FF3300',
+        'blue-link': '#003399',
+        'bluish-link': '#03C',
+        'orange-hover': '#F60'
+      },
+      spacing: {
+        '5.5': '1.375rem',
+        '7': '1.75rem',
+        '11': '2.7rem',
+        '15': '3.75rem',
+        '47': '11.75rem',
+        '116': '29rem'
+      },
+      lineHeight: {
+        '5.5': '1.375rem',
+        '11': '2.7rem', 
+      }
+    },
   },
   variants: {},
   plugins: [],
